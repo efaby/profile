5516d4ef88547e6074a0648d573a84161affe93f
efaby <efaby10@gmail.com>
2025-06-02 17:19:54 -0500

    feat: test redirect

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/profile/",
+  base: "/profile/",
   server: {
     host: "::",
     port: 8080,
